21ed12dfda2b7e3bd344d59ba46c8c4e7070b961
Edward Angelo Adonis <mtoyadonis79@gmail.com>
2025-12-04 19:50:59 +0800

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Portfolio } from './portfolio/portfolio';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+
+export const routes: Routes = [
+    {path:'', component: Home},
+    {path:'portfolio', component: Portfolio},
+    {path:'about', component: About},
+    {path:'contact', component: Contact},
+];
---
+++ src/app/portfolio/portfolio.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [RouterModule],
+  templateUrl: './portfolio.html',
+  styleUrl: './portfolio.css',
+})
+export class Portfolio {
+
+}
